ad19195037eca2087a679839da95cbe1c6085ac0
Alexander Kochemasov <alexander.kochemasov@litota.ru>
2017-10-12 03:13:40 +0400

    add brain calc and refactor structure

---
+++ src/games/even.js
@@ -0,0 +1,16 @@
+import init from '..';
+
+const getRndInteger = () => Math.floor(Math.random() * 100) + 1;
+const getQuestion = () => getRndInteger();
+
+const isEven = num => num % 2 === 0;
+
+const getAnswerCorrect = question => (isEven(question) ? 'yes' : 'no');
+
+const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  init(getQuestion, getAnswerCorrect);
+};
+
+export default brainEven;
