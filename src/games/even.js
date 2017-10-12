20d6873b0766c2cea8be74e06ac83789c7699c38
Alexander Kochemasov <alexander.kochemasov@litota.ru>
2017-10-13 00:51:21 +0400

    refactor even

--- src/games/even.js
+++ src/games/even.js
@@ -1,16 +1,21 @@
 import init from '..';
 
+const desc = 'Answer "yes" if number even otherwise answer "no".';
+
 const getRndInteger = () => Math.floor(Math.random() * 100) + 1;
 const getQuestion = () => getRndInteger();
 
 const isEven = num => num % 2 === 0;
-
 const getAnswerCorrect = question => (isEven(question) ? 'yes' : 'no');
 
+const generateTask = () => {
+  const question = getQuestion();
+  const answerCorrect = getAnswerCorrect(question);
+  return [question, answerCorrect];
+};
+
 const game = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no".');
-  init(getQuestion, getAnswerCorrect);
+  init(generateTask, desc);
 };
 
 export default game;
